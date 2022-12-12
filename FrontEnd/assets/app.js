c35a1bc141857a61944581ce4a8c3b1e995ef6ee
Ugo_Laptop <pulito.ugo@gmail.com>
2022-12-12 18:17:40 +0100

    filtrage des projets

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,22 +1,29 @@
-let gallery = document.querySelector('.gallery')
-let filters = document.querySelector('.filters')
-let categories = new Set();
+let gallery, filters, filtersList, categories, figure, figures, image, caption, activeFilter, button;
+gallery = document.querySelector('.gallery');
+filtersList = document.querySelector('.filters');
+categories = new Set();
+
 function initFilters(text){
-    let button = document.createElement('button');
-    filters.appendChild(button);
+    button = document.createElement('button');
+    filtersList.appendChild(button);
     button.classList.add('filter');
     button.innerText = text;
 }
 function changeActiveFilter(){
-    let activeFilter = document.querySelector('.filter.selected');
-    if (this.classList.contains('selected')){
-        console.log('filtre déjà sélectionné')
-    }
-    else{
+    if (!this.classList.contains('selected')){
         activeFilter.classList.remove('selected');
         this.classList.add('selected');
         activeFilter = this;
-        console.log('filtre sélectionné : '+ activeFilter.innerText);
+    }
+}
+function filterProjects(){
+    for (let item of figures){
+        //Affichage de tous les projets
+        item.removeAttribute('style');
+        //Puis filtrage selon la catégorie sélectionnée
+        if(activeFilter.innerText !== 'Tous' && item.dataset.category !== activeFilter.innerText){
+            item.style.display = 'none';
+        }
     }
 }
 fetch('http://localhost:5678/api/works')
@@ -25,25 +32,32 @@ fetch('http://localhost:5678/api/works')
     })
     .then((data) => {
         //Création de la galerie
-        for (var i = 0; i < data.length; i++){
-            let figure = document.createElement('figure');
-            let image = document.createElement('img');
-            let caption = document.createElement('figcaption');
+        for (let item of data){
+            figure = document.createElement('figure');
+            image = document.createElement('img');
+            caption = document.createElement('figcaption');
             gallery.appendChild(figure);
             figure.appendChild(image);
             figure.appendChild(caption);
-            caption.innerText = data[i].title;
-            image.setAttribute('src', data[i].imageUrl);
+            figure.dataset.category = item.category.name;
+            caption.innerText = item.title;
+            image.setAttribute('src', item.imageUrl);
             image.setAttribute('crossorigin', 'anonymous');
-            image.setAttribute('alt', data[i].title);
-            categories.add(data[i].category.name);
+            image.setAttribute('alt', item.title);
         }
         //Création des filtres
-        initFilters('Tous');
-        filters.firstElementChild.classList.add('selected');
-        for (let category of categories){
-            initFilters(category);
+        for (let item of data){
+            categories.add(item.category.name);
         }
-        let filter = document.querySelectorAll('.filter');
-        filter.forEach(item => {item.addEventListener('click', changeActiveFilter)});
-    });
+        initFilters('Tous');
+        filtersList.firstElementChild.classList.add('selected');
+        activeFilter = document.querySelector('.filter.selected');
+        for (let item of categories){
+            initFilters(item);
+        }
+        filters = document.querySelectorAll('.filter');
+        filters.forEach(filter => {filter.addEventListener('click', changeActiveFilter)});
+        //Filtrage des projets
+        figures = document.querySelectorAll('.gallery figure');
+        filters.forEach(filter => {filter.addEventListener('click', filterProjects)});
+    })
