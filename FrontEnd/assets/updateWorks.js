4d8464434479ffa35ab80b4b28c6ca2da294df84
ugo_pc <pulito.ugo@gmail.com>
2022-12-19 17:03:09 +0100

    ajout topbar + boutons edit

--- FrontEnd/assets/updateWorks.js
+++ FrontEnd/assets/updateWorks.js
@@ -6,11 +6,29 @@ function getCookie(name){
 }
 function logout(){
     document.cookie = 'token' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    window.location.reload
+    location.reload()
 }
-
+function displayTopBarEditMode(){
+    const topBar = document.createElement('div');
+    const editMsg = document.createElement('p');
+    const editButton = document.createElement('button');
+    topBar.className = 'topbar'
+    editMsg.innerHTML = '<i class="edit-icon"></i>Mode édition';
+    editButton.textContent = "publier les changements";
+    document.querySelector('html').insertBefore(topBar, document.querySelector('body'));
+    topBar.appendChild(editMsg);
+    topBar.appendChild(editButton);
+}
+function displayButtonEditMode(){
+    editButton = document.createElement('button');
+    editButton.className = 'edit'
+    editButton.innerHTML='<i class="edit-icon"></i>Modifier';
+    document.querySelector('#introduction article').insertBefore(editButton, document.querySelector('h2'));
+}
 if(getCookie('token')){
     console.log('Utilisateur connecté');
     document.querySelector('nav li:nth-child(3)').textContent = 'logout';
-    document.querySelector('nav li:nth-child(3)').addEventListener('click', logout)
+    document.querySelector('nav li:nth-child(3)').addEventListener('click', logout);
+    displayTopBarEditMode();
+    displayButtonEditMode();
 }
