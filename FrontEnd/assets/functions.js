9f6d4c7328fa8f002c7b96e3d3fd717ff3542813
ugo_pc <pulito.ugo@gmail.com>
2022-12-22 16:10:36 +0100

    extraction fonctions annexes

---
+++ FrontEnd/assets/functions.js
@@ -0,0 +1,32 @@
+function displayLogout(){
+    document.querySelector('#login-out').textContent = 'logout';
+    document.querySelector('#login-out').addEventListener('click', function logout(){
+        document.cookie = 'token' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        location.reload()
+    });
+}
+
+function getCookie(name){
+    const data = '; '+document.cookie;
+    const cookies = data.split('; '+name+'=').pop();
+    const value = cookies.split('; ').shift();
+    return value
+}
+
+function displayEditBtns(){
+    function createEditBtn(constName, idName){
+        constName.classList.add('edit-btn', idName);
+        constName.id = idName;
+        constName.innerHTML = '<svg class="edit-icon"><use href="./assets/icons/edit-icon.svg#edit-icon-black"></use></svg>Modifier';}
+    const introEditBtn = document.createElement('button');
+    const heroImgEditBtn = document.createElement('button');
+    const worksEditBtn = document.createElement('button');
+    createEditBtn(introEditBtn, 'intro-edit')
+    document.querySelector('#introduction article').insertBefore(introEditBtn, document.querySelector('h2'));
+    createEditBtn(heroImgEditBtn, 'hero-img-edit');
+    document.querySelector('#introduction figure img').insertAdjacentElement('afterend', heroImgEditBtn);
+    createEditBtn(worksEditBtn, 'works-edit');
+    document.querySelector('#portfolio h2').insertAdjacentElement('afterend', worksEditBtn);
+}
+
+export {displayLogout, getCookie, displayEditBtns};
